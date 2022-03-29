6e740bab3379a312a17ad7fb8a26520a132b9598
berkayk2 <berkayk2@illinois.edu>
2022-03-28 22:35:11 -0600

    Did the final polishings for USBConnection. It should function fine now

--- Android Application/app/src/main/java/com/example/spliceApp/USBConnection.kt
+++ Android Application/app/src/main/java/com/example/spliceApp/USBConnection.kt
@@ -9,7 +9,6 @@ import android.hardware.usb.UsbManager
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -17,7 +16,6 @@ import com.hoho.android.usbserial.driver.*
 import com.hoho.android.usbserial.util.SerialInputOutputManager
 import spliceApp.BuildConfig
 import java.io.IOException
-import java.nio.ByteBuffer
 
 
 // Use applicationContext for the context variable
@@ -86,14 +84,10 @@ class USBConnection(private val context: Context) : SerialInputOutputManager.Lis
     @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onNewData(data: ByteArray?) {
         mainLooper.post {
-            if (data != null && data.isNotEmpty()) {
-                val buffer = ByteBuffer.wrap(data)
-                try {
-                    //val float1 = buffer.getFloat(0).times(1000000000.0).toString().substring(0, 5)
-                    val float1 = data.decodeToString()
-                    Log.e("onNewData:", float1)
+            if (data != null) {
+                val float1 = data.decodeToString().trim()
+                if (float1.indexOf('.') != -1 && float1.length == 4) {
                     this.uwbDistanceText.text = float1
-                } catch (e: java.lang.Exception) {
                 }
             }
         }
